3799424e3b6f5acbab46763b2682307678dcdb6b
kkiyama117 <k.kiyama117@gmail.com>
2019-02-18 16:18:35 +0900

    Copy old dir file

---
+++ enterlist/get_data.py
@@ -0,0 +1,16 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def get_personal_data():
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        '../tomiken-670c5e3660b3.json', scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open_by_key('19DsLifB7GZcA13NdroXPWpS9WtUIcgy6KcidC6wGhOY').sheet1
+
+    wks.update_acell('A1', 'Hello World!')
+    answer1 = wks.acell('C3')
+    return answer1
