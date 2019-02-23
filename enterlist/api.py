a66e556409398fac1d6e444d3d401f898f0c5de8
kkiyama117 <k.kiyama117@gmail.com>
2019-02-23 22:21:58 +0900

    ?

---
+++ enterlist/api.py
@@ -0,0 +1,28 @@
+from spread.get_data import GetDataManager
+from spread.models import format_enter
+
+
+def request(slack_id: str, message: str):
+    gspread_manager = GetDataManager()
+    command, args = parse_command(message, slack_id)
+    if command is not None:
+        enter_data = getattr(gspread_manager, command)(args)
+        return format_enter(enter_data)
+    else:
+        return "No command found!"
+
+
+def parse_command(message: str, slack_id: str):
+    """Slack message 解析"""
+    commands = message.split()
+    main_command = commands[0]
+    args = None
+    if main_command == 'check':
+        main_command = 'get'
+        args = {'row': commands[1]}
+    elif main_command == 'check_all':
+        main_command = 'get_all'
+        args = {'slack_id': slack_id}
+    else:
+        main_command = None
+    return main_command, args
