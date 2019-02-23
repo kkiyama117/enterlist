6c2ec758c93b00c046f86e516451619b0529529f
kkiyama117 <k.kiyama117@gmail.com>
2019-02-23 13:32:56 +0900

    Add example

--- enterlist/__version__.py
+++ enterlist/__version__.py
@@ -1,8 +1,8 @@
 __title__ = 'enterlist'
 __description__ = 'nothing.'
 __url__ = 'https://github.com/kkiyama117/KUEventParser'
-__version__ = '0.0.1'
-__build__ = 0x000001
+__version__ = '1.0.1'
+__build__ = 0x010001
 __author__ = 'kkiyama117'
 __author_email__ = 'k.kiyama117@gmail.com'
 __maintainer__ = ' kkiyama117'
